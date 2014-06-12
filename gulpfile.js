2b28a2896f72e80af86a77a7386fecf98000a692
Chris Ferdinandi <hello@gomakethings.com>
2014-06-11 23:49:29 -0400

    Updated GulpJS variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,96 +1,97 @@
-var gulp    = require('gulp'),
-    karma   = require('gulp-karma'),
-    jshint  = require('gulp-jshint'),
-    stylish = require('jshint-stylish'),
-    header  = require('gulp-header'),
-    uglify  = require('gulp-uglify'),
-    plumber = require('gulp-plumber'),
-    clean   = require('gulp-clean'),
-    rename  = require('gulp-rename'),
-    sass = require('gulp-sass'),
-    prefix = require('gulp-autoprefixer'),
-    concat = require('gulp-concat'),
-    minify = require('gulp-minify-css'),
-    package = require('./package.json');
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var clean = require('gulp-clean');
+var rename = require('gulp-rename');
+var header = require('gulp-header');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var minify = require('gulp-minify-css');
+var karma = require('gulp-karma');
+var package = require('./package.json');
 
 var paths = {
-  output : 'dist/',
-  scripts : {
-    input: [ 'src/**/*.js' ],
-    output: 'dist/js/'
-  },
-  styles : {
-    input: 'src/sass/*.scss',
-    output: 'dist/css/'
-  },
-  test: [ 'test/spec/**/*.js' ]
+	output : 'dist/',
+	scripts : {
+		input: [ 'src/**/*.js' ],
+		output: 'dist/js/'
+	},
+	styles : {
+		input: 'src/sass/*.scss',
+		output: 'dist/css/'
+	},
+	test: [ 'test/spec/**/*.js' ]
 };
 
 var banner =
-  '/**\n' +
-  ' * <%= package.name %> v<%= package.version %>\n' +
-  ' * <%= package.description %>, by <%= package.author.name %>.\n' +
-  ' * <%= package.repository.url %>\n' +
-  ' * \n' +
-  ' * Free to use under the MIT License.\n' +
-  ' * http://gomakethings.com/mit/\n' +
-  ' */\n\n';
+	'/**\n' +
+	' * <%= package.name %> v<%= package.version %>\n' +
+	' * <%= package.description %>, by <%= package.author.name %>.\n' +
+	' * <%= package.repository.url %>\n' +
+	' * \n' +
+	' * Free to use under the MIT License.\n' +
+	' * http://gomakethings.com/mit/\n' +
+	' */\n\n';
 
 var bannerMin =
-  '/**' +
-  ' <%= package.name %> v<%= package.version %>' +
-  ' | <%= package.repository.url %>' +
-  ' | MIT Licensed http://gomakethings.com/mit/' +
-  ' */\n';
+	'/**' +
+	' <%= package.name %> v<%= package.version %>, by Chris Ferdinandi.' +
+	' <%= package.repository.url %>.' +
+	' Licensed under MIT: http://gomakethings.com/mit/.' +
+	' */\n';
 
 gulp.task('scripts', ['clean'], function() {
-  return gulp.src(paths.scripts.input)
-    .pipe(plumber())
-    .pipe(header(banner, { package : package }))
-    .pipe(gulp.dest(paths.scripts.output))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(uglify())
-    .pipe(header(bannerMin, { package : package }))
-    .pipe(gulp.dest(paths.scripts.output));
+	return gulp.src(paths.scripts.input)
+		.pipe(plumber())
+		.pipe(concat('main.js'))
+		.pipe(header(banner, { package : package }))
+		.pipe(gulp.dest(paths.scripts.output))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(uglify())
+		.pipe(header(bannerMin, { package : package }))
+		.pipe(gulp.dest(paths.scripts.output));
 });
 
 gulp.task('styles', ['clean'], function() {
-  return gulp.src(paths.styles.input)
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(prefix('last 2 version', '> 1%'))
-    .pipe(header(banner, { package : package }))
-    .pipe(gulp.dest(paths.styles.output))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(minify())
-    .pipe(header(bannerMin, { package : package }))
-    .pipe(gulp.dest(paths.styles.output));
+	return gulp.src(paths.styles.input)
+		.pipe(plumber())
+		.pipe(sass())
+		.pipe(prefix('last 2 version', '> 1%'))
+		.pipe(header(banner, { package : package }))
+		.pipe(gulp.dest(paths.styles.output))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(minify())
+		.pipe(header(bannerMin, { package : package }))
+		.pipe(gulp.dest(paths.styles.output));
 });
 
 gulp.task('lint', function () {
-  return gulp.src(paths.scripts.input)
-    .pipe(plumber())
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
+	return gulp.src(paths.scripts.input)
+		.pipe(plumber())
+		.pipe(jshint())
+		.pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('clean', function () {
-  return gulp.src(paths.output, { read: false })
-    .pipe(plumber())
-    .pipe(clean());
+	return gulp.src(paths.output, { read: false })
+		.pipe(plumber())
+		.pipe(clean());
 });
 
 gulp.task('test', function() {
-  return gulp.src(paths.scripts.input.concat(paths.test))
-    .pipe(plumber())
-    .pipe(karma({ configFile: 'test/karma.conf.js' }))
-    .on('error', function(err) { throw err; });
+	return gulp.src(paths.scripts.input.concat(paths.test))
+		.pipe(plumber())
+		.pipe(karma({ configFile: 'test/karma.conf.js' }))
+		.on('error', function(err) { throw err; });
 });
 
 gulp.task('default', [
-  'lint',
-  'clean',
-  'scripts',
-  'styles',
-  'test'
+	'lint',
+	'clean',
+	'scripts',
+	'styles',
+	'test'
 ]);
