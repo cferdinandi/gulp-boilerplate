228c0bb5663efe58b86b0d07d945e18685fa736d
Chris Ferdinandi <hello@gomakethings.com>
2014-06-15 22:38:53 -0400

    Updated version number

--- dist/css/myplugin.css
+++ dist/css/myplugin.css
@@ -1,5 +1,5 @@
 /**
- * gulp-boilerplate v0.0.2
+ * gulp-boilerplate v0.0.3
  * My Gulp.js boilerplate for creating new web projects, by Chris Ferdinandi.
  * http://github.com/cferdinandi/Plugin
  * 
