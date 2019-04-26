0a69563a6ddef8437ee9f4c3fae7032c6e8aacca
Michael Gale <michael.gale@outlook.com.au>
2019-04-26 16:37:49 +1000

    Update gulpfile.js
    Avoid gulp errors if package repository is undefined.

--- gulpfile.js
+++ gulpfile.js
@@ -53,14 +53,14 @@ var banner = {
 		' * <%= package.description %>\n' +
 		' * (c) ' + new Date().getFullYear() + ' <%= package.author.name %>\n' +
 		' * <%= package.license %> License\n' +
-		' * <%= package.repository.url %>\n' +
+		(package.repository) ? ' * <%= package.repository.url %>\n' : '' +
 		' */\n\n',
 	min:
 		'/*!' +
 		' <%= package.name %> v<%= package.version %>' +
 		' | (c) ' + new Date().getFullYear() + ' <%= package.author.name %>' +
 		' | <%= package.license %> License' +
-		' | <%= package.repository.url %>' +
+		(package.repository) ? ' * <%= package.repository.url %>\n' : '' +
 		' */\n'
 };
 
